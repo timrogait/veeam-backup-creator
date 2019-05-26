def98668480cf7794da238542996d2dc8df7d878
timrogait <timrogait@gmx.de>
2019-05-26 22:14:53 +0200

    add progress bar

--- veeam-backup-creator.ps1
+++ veeam-backup-creator.ps1
@@ -4,35 +4,25 @@ Add-Type -AssemblyName System.Drawing
 
 #Base Form
 $Form = New-Object system.Windows.Forms.Form
-$Form.ClientSize = '400,500'
+$Form.ClientSize = '400,530'
 $Form.text = "Veeam Database Backup"
 $Form.TopMost = $true
 $Form.AutoSize = $false
 $Form.FormBorderStyle = 'FixedDialog'
 
 $GB_Backup1 = New-Object system.Windows.Forms.Groupbox
-$GB_Backup1.height = 190
+$GB_Backup1.height = 155
 $GB_Backup1.width = 378
 $GB_Backup1.text = "Choose the Databases you want to backup:"
 $GB_Backup1.location = New-Object System.Drawing.Point(11, 10)
 $GB_Backup1.Font = 'Microsoft Sans Serif,10'
 
-#Chosse Backup Label
-<#
-$L_SelectBackup1 = New-Object system.Windows.Forms.Label
-$L_SelectBackup1.text = "Choose the Databases you want to backup: "
-$L_SelectBackup1.AutoSize = $true
-$L_SelectBackup1.width = 25
-$L_SelectBackup1.height = 10
-$L_SelectBackup1.location = New-Object System.Drawing.Point(11, 20)
-$L_SelectBackup1.Font = 'Microsoft Sans Serif,10'
-#>
 #VBR Backup Checkbox
 $CB_VBR1 = New-Object system.Windows.Forms.Checkbox
 $CB_VBR1.text = "Veeam Backup and Replication"
 $CB_VBR1.width = 250
 $CB_VBR1.height = 25
-$CB_VBR1.location = New-Object System.Drawing.Point(11, 35)
+$CB_VBR1.location = New-Object System.Drawing.Point(11, 30)
 $CB_VBR1.Font = 'Microsoft Sans Serif,10'
 
 #Veeam Enterprise Manager Backup Checkbox
@@ -40,7 +30,7 @@ $CB_VEM1 = New-Object system.Windows.Forms.Checkbox
 $CB_VEM1.text = "Veeam Enterprise Manager"
 $CB_VEM1.width = 250
 $CB_VEM1.height = 25
-$CB_VEM1.location = New-Object System.Drawing.Point(11, 75)
+$CB_VEM1.location = New-Object System.Drawing.Point(11, 60)
 $CB_VEM1.Font = 'Microsoft Sans Serif,10'
 
 #Veeam ONE Backup Checkbox
@@ -48,7 +38,7 @@ $CB_VONE1 = New-Object system.Windows.Forms.Checkbox
 $CB_VONE1.text = "Veeam ONE"
 $CB_VONE1.width = 250
 $CB_VONE1.height = 25
-$CB_VONE1.location = New-Object System.Drawing.Point(11, 115)
+$CB_VONE1.location = New-Object System.Drawing.Point(11, 90)
 $CB_VONE1.Font = 'Microsoft Sans Serif,10'
 
 #Veeam Configuration Backup
@@ -56,24 +46,14 @@ $CB_Conf1 = New-Object system.Windows.Forms.Checkbox
 $CB_Conf1.text = "Veeam Configuration Backup"
 $CB_Conf1.width = 250
 $CB_Conf1.height = 25
-$CB_Conf1.location = New-Object System.Drawing.Point(11, 155)
+$CB_Conf1.location = New-Object System.Drawing.Point(11, 120)
 $CB_Conf1.Font = 'Microsoft Sans Serif,10'
 
-<#
-$L_Divider1 = New-Object system.Windows.Forms.Label
-$L_Divider1.text = ""
-$L_Divider1.AutoSize = $false
-$L_Divider1.BorderStyle = "Fixed3d"
-$L_Divider1.width = 378
-$L_Divider1.height = 2
-$L_Divider1.location = New-Object System.Drawing.Point(11, 220)
-#>
-
 $GB_Backup2 = New-Object system.Windows.Forms.Groupbox
-$GB_Backup2.height = 75
+$GB_Backup2.height = 70
 $GB_Backup2.width = 378
 $GB_Backup2.text = "Choose the Backup destination:"
-$GB_Backup2.location = New-Object System.Drawing.Point(11, 210)
+$GB_Backup2.location = New-Object System.Drawing.Point(11, 180)
 $GB_Backup2.Font = 'Microsoft Sans Serif,10'
 
 $L_SelectFolder1 = New-Object system.Windows.Forms.Label
@@ -93,10 +73,10 @@ $BU_Folder1.location = New-Object System.Drawing.Point(340, 35)
 $BU_Folder1.Font = 'Microsoft Sans Serif,10'
 
 $GB_Backup3 = New-Object system.Windows.Forms.Groupbox
-$GB_Backup3.height = 115
+$GB_Backup3.height = 110
 $GB_Backup3.width = 378
 $GB_Backup3.text = "Choose SQL Database Authentication:"
-$GB_Backup3.location = New-Object System.Drawing.Point(11, 295)
+$GB_Backup3.location = New-Object System.Drawing.Point(11, 265)
 $GB_Backup3.Font = 'Microsoft Sans Serif,10'
 
 #Windows Database Authentication
@@ -105,7 +85,7 @@ $RB_WinAuth1.text = "Windows Authentication"
 $RB_WinAuth1.AutoSize = $true
 $RB_WinAuth1.width = 25
 $RB_WinAuth1.height = 10
-$RB_WinAuth1.location = New-Object System.Drawing.Point(11, 35)
+$RB_WinAuth1.location = New-Object System.Drawing.Point(11, 30)
 $RB_WinAuth1.Font = 'Microsoft Sans Serif,10'
 $RB_WinAuth1.Checked = $true
 
@@ -115,7 +95,7 @@ $RB_SQLAuth1.text = "SQL Authentication"
 $RB_SQLAuth1.AutoSize = $true
 $RB_SQLAuth1.width = 25
 $RB_SQLAuth1.height = 10
-$RB_SQLAuth1.location = New-Object System.Drawing.Point(211, 35)
+$RB_SQLAuth1.location = New-Object System.Drawing.Point(211, 30)
 $RB_SQLAuth1.Font = 'Microsoft Sans Serif,10'
 $RB_SQLAuth1.Checked = $false
 
@@ -124,7 +104,7 @@ $TB_UserName1.text = "Input Username"
 $TB_UserName1.AutoSize = $false
 $TB_UserName1.width = 130
 $TB_UserName1.height = 20
-$TB_UserName1.location = New-Object System.Drawing.Point(11, 75)
+$TB_UserName1.location = New-Object System.Drawing.Point(11, 70)
 $TB_UserName1.Font = 'Microsoft Sans Serif,10'
 $TB_UserName1.Enabled =$false
 
@@ -133,32 +113,49 @@ $TB_Password1.text = "Input Password"
 $TB_Password1.AutoSize = $false
 $TB_Password1.width = 130
 $TB_Password1.height = 20
-$TB_Password1.location = New-Object System.Drawing.Point(152, 75)
+$TB_Password1.location = New-Object System.Drawing.Point(152, 70)
 $TB_Password1.Font = 'Microsoft Sans Serif,10'
 $TB_Password1.Enabled =$false
+
+$GB_Backup4 = New-Object system.Windows.Forms.Groupbox
+$GB_Backup4.height = 70
+$GB_Backup4.width = 378
+$GB_Backup4.text = "Status:"
+$GB_Backup4.location = New-Object System.Drawing.Point(11, 385)
+$GB_Backup4.Font = 'Microsoft Sans Serif,10'
+
+$PB_Status1 = New-Object System.Windows.Forms.ProgressBar
+$PB_Status1.Maximum = 100
+$PB_Status1.Minimum = 0
+$PB_Status1.height = 20
+$PB_Status1.width = 358
+$PB_Status1.Location = New-Object System.Drawing.Point(11, 30)
+$PB_Status1.Value = 0
+$PB_Status1.Style = 'Continuous'
 
 $BU_Start1 = New-Object system.Windows.Forms.Button
 $BU_Start1.text = "Start"
 $BU_Start1.width = 80
-$BU_Start1.height = 30
-$BU_Start1.location = New-Object System.Drawing.Point(210, 450)
+$BU_Start1.height = 25
+$BU_Start1.location = New-Object System.Drawing.Point(219, 490)
 $BU_Start1.Font = 'Microsoft Sans Serif,10'
 
 $BU_Cancel1 = New-Object system.Windows.Forms.Button
 $BU_Cancel1.text = "Cancel"
 $BU_Cancel1.width = 80
-$BU_Cancel1.height = 30
-$BU_Cancel1.location = New-Object System.Drawing.Point(300, 450)
+$BU_Cancel1.height = 25
+$BU_Cancel1.location = New-Object System.Drawing.Point(309, 490)
 $BU_Cancel1.Font = 'Microsoft Sans Serif,10'
 
 $Global:BackupFolder = $null
 $Global:BackupCounter = "False" #Disposes Window only if Backup was successfull
 $Global:Date = Get-Date -f "dd.MM.yyyy_HH.mm.ss"
 
-$Form.controls.AddRange(@($GB_Backup1, $L_SelectBackup1, $CB_VBR1, $CB_VEM1, $CB_VONE1, $CB_Conf1, $L_Divider1, $GB_Backup2, $L_SelectFolder1, $BU_Folder1, $GB_Backup3, $RB_WinAuth1, $RB_SQLAuth1, $TB_UserName1, $TB_Password1, $BU_Start1, $BU_Cancel1))
+$Form.controls.AddRange(@($GB_Backup1, $L_SelectBackup1, $CB_VBR1, $CB_VEM1, $CB_VONE1, $CB_Conf1, $L_Divider1, $GB_Backup2, $L_SelectFolder1, $BU_Folder1, $GB_Backup3, $RB_WinAuth1, $RB_SQLAuth1, $TB_UserName1, $TB_Password1, $GB_Backup4, $PB_Status1, $BU_Start1, $BU_Cancel1))
 $GB_Backup1.controls.AddRange(@($L_SelectBackup1, $CB_VBR1, $CB_VEM1, $CB_VONE1, $CB_Conf1))
 $GB_Backup2.controls.AddRange(@($L_SelectFolder1, $BU_Folder1))
 $GB_Backup3.controls.AddRange(@($RB_WinAuth1, $RB_SQLAuth1, $TB_UserName1, $TB_Password1))
+$GB_Backup4.controls.Add($PB_Status1)
 
 #If SQL Auth Checkbox is checked, Username and Password input appear
 $RB_WinAuth1.Add_CheckedChanged( {
@@ -193,17 +190,18 @@ $BU_Folder1.Add_Click( {
         [void]$BD_ChooseFolder.ShowDialog()
         $Global:BackupFolder = $BD_ChooseFolder.SelectedPath
         if (!$Global:BackupFolder) {
-            $L_SelectFolder1.Text = "No folder selected"
+            $L_SelectFolder1.Text = "No Backup folder selected"
         }
         else {
-            $L_SelectFolder1.Text = "Backup folder selected"
+            $L_SelectFolder1.Text = [string]$Global:BackupFolder
         }
     })
 $BU_Start1.Add_Click( {
-        $BU_Start1.Text = "Running..."
+        #$BU_Start1.Text = "Running..."
+        $PB_Status1.Value = 10
         if ($RB_WinAuth1.Checked -eq "True") {
             #Backup with Win Auth
-            if (!$CB_VBR1.Checked -and !$CB_VEM1.Checked -and !$CB_VONE1.Checked) {
+            if (!$CB_VBR1.Checked -and !$CB_VEM1.Checked -and !$CB_VONE1.Checked -and !$CB_Conf1.Checked) {
                 #Nothing checked for backup
                 [System.Windows.Forms.MessageBox]::Show("No Veeam Database selected!", "Warning", 0, [System.Windows.Forms.MessageBoxIcon]::Exclamation)
             }
@@ -222,6 +220,7 @@ $BU_Start1.Add_Click( {
                     try {
                         Invoke-Sqlcmd -ErrorAction Stop -ServerInstance $VBRSQLServerName\$VBRSQLInstanceName -Database $VBRSQLDBName -Query $BackupQuery 
                         $Global:BackupCounter = "True"
+                        $PB_Status1.Value = 25
                         [System.Windows.Forms.MessageBox]::Show("Veeam BR backup created!", "Sucess", 0, [System.Windows.Forms.MessageBoxIcon]::Information)            
                     }
                     catch {
@@ -243,6 +242,7 @@ $BU_Start1.Add_Click( {
                     try {
                         Invoke-Sqlcmd -ErrorAction Stop -ServerInstance $VEMSQLServerName\$VEMSQLInstanceName -Database $VEMSQLDBName -Query $BackupQuery
                         $Global:BackupCounter = "True"
+                        $PB_Status1.Value = 50
                         [System.Windows.Forms.MessageBox]::Show("Veeam EM backup created!", "Sucess", 0, [System.Windows.Forms.MessageBoxIcon]::Information)               
                     }
                     catch {
@@ -263,6 +263,7 @@ $BU_Start1.Add_Click( {
                     try {
                         Invoke-Sqlcmd -ErrorAction Stop -ServerInstance $VONESQLServerAndInstanceName -Database $VONESQLDBName -Query $BackupQuery
                         $Global:BackupCounter = "True"
+                        $PB_Status1.Value = 75
                         [System.Windows.Forms.MessageBox]::Show("Veeam ONE backup created!", "Sucess", 0, [System.Windows.Forms.MessageBoxIcon]::Information)               
                     }
                     catch {
@@ -273,17 +274,40 @@ $BU_Start1.Add_Click( {
                     [System.Windows.Forms.MessageBox]::Show("No Veeam ONE installation found in registry!", "Error", 0, [System.Windows.Forms.MessageBoxIcon]::Exclamation)        
                 }
             }
+            if ($CB_Conf1.Checked -eq "True") {
+                try {
+                    $CurrentConfigBackup = Get-VBRConfigurationBackupJob
+                    $CurrentRepository = $CurrentConfigBackup.Target
+                    $LocalServer = Get-VBRServer | Where-Object {($_.Type -eq "Local") -and ($_.Description -eq "Backup server")}
+                    $TempRepository = Add-VBRBackupRepository -Name "Temp_Repo_$(get-date -f dd.MM.yyyy_HH.mm.ss)" -Server $LocalServer -Folder $Global:BackupFolder -Type WinLocal
+
+                    Set-VBRConfigurationBackupJob -Enable:$true -Repository $TempRepository
+                    Start-VBRConfigurationBackupJob
+                    Set-VBRConfigurationBackupJob -Repository $CurrentRepository
+                    Remove-VBRBackupRepository -Repository $TempRepository -Confirm:$false
+                    $Global:BackupCounter = "True"
+                    $PB_Status1.Value = 90
+                    [System.Windows.Forms.MessageBox]::Show("Veeam Configuration backup created!", "Sucess", 0, [System.Windows.Forms.MessageBoxIcon]::Information)               
+                }
+                catch {
+                    [System.Windows.Forms.MessageBox]::Show("Unable to create Veeam Configuration backup", "Error", 0, [System.Windows.Forms.MessageBoxIcon]::Exclamation)        
+                }
+            }
             if ($Global:BackupCounter -eq "True") {
+                $PB_Status1.Value = 100
+                Start-Sleep -Seconds 1.5
                 $Form.Dispose()
             }
             else {
-                $BU_Start1.Text = "Start"
+                #$BU_Start1.Text = "Start"
+                $PB_Status1.Value = 0
             }
         }
         else {
             $UserName = $TB_UserName1.Text
             $Password = $TB_Password1.Text
-            $BU_Start1.Text = "Running..."
+            #$BU_Start1.Text = "Running..."
+            $PB_Status1.Value = 10
             if (!$CB_VBR1.Checked -and !$CB_VEM1.Checked -and !$CB_VONE1.Checked) {
                 [System.Windows.Forms.MessageBox]::Show("No Veeam Database selected!", "Warning", 0, [System.Windows.Forms.MessageBoxIcon]::Exclamation)
             }
@@ -301,6 +325,7 @@ $BU_Start1.Add_Click( {
                     try {
                         Invoke-Sqlcmd -ErrorAction Stop -ServerInstance $VBRSQLServerName\$VBRSQLInstanceName -Database $VBRSQLDBName -Username $UserName -Password $Password -Query $BackupQuery 
                         $Global:BackupCounter = "True"
+                        $PB_Status1.Value = 25
                         [System.Windows.Forms.MessageBox]::Show("Veeam BR backup created!", "Sucess", 0, [System.Windows.Forms.MessageBoxIcon]::Information)            
                     }
                     catch {
@@ -322,6 +347,7 @@ $BU_Start1.Add_Click( {
                     try {
                         Invoke-Sqlcmd -ErrorAction Stop -ServerInstance $VEMSQLServerName\$VEMSQLInstanceName -Database $VEMSQLDBName -Username $UserName -Password $Password -Query $BackupQuery
                         $Global:BackupCounter = "True"
+                        $PB_Status1.Value = 50
                         [System.Windows.Forms.MessageBox]::Show("Veeam EM backup created!", "Sucess", 0, [System.Windows.Forms.MessageBoxIcon]::Information)               
                     }
                     catch {
@@ -342,6 +368,7 @@ $BU_Start1.Add_Click( {
                     try {
                         Invoke-Sqlcmd -ErrorAction Stop -ServerInstance $VONESQLServerAndInstanceName -Database $VONESQLDBName -Username $UserName -Password $Password -Query $BackupQuery
                         $Global:BackupCounter = "True"
+                        $PB_Status1.Value = 75
                         [System.Windows.Forms.MessageBox]::Show("Veeam ONE backup created!", "Sucess", 0, [System.Windows.Forms.MessageBoxIcon]::Information)               
                     }
                     catch {
@@ -352,11 +379,33 @@ $BU_Start1.Add_Click( {
                     [System.Windows.Forms.MessageBox]::Show("No Veeam ONE installation found in registry!", "Error", 0, [System.Windows.Forms.MessageBoxIcon]::Exclamation)        
                 }
             }
+            if ($CB_Conf1.Checked -eq "True") {
+                try {
+                    $CurrentConfigBackup = Get-VBRConfigurationBackupJob
+                    $CurrentRepository = $CurrentConfigBackup.Target
+                    $LocalServer = Get-VBRServer | Where-Object {($_.Type -eq "Local") -and ($_.Description -eq "Backup server")}
+                    $TempRepository = Add-VBRBackupRepository -Name "Temp_Repo_$(get-date -f dd.MM.yyyy_HH.mm.ss)" -Server $LocalServer -Folder $Global:BackupFolder -Type WinLocal
+
+                    Set-VBRConfigurationBackupJob -Enable:$true -Repository $TempRepository
+                    Start-VBRConfigurationBackupJob
+                    Set-VBRConfigurationBackupJob -Repository $CurrentRepository
+                    Remove-VBRBackupRepository -Repository $TempRepository -Confirm:$false
+                    $Global:BackupCounter = "True"
+                    $PB_Status1.Value = 90
+                    [System.Windows.Forms.MessageBox]::Show("Veeam Configuration backup created!", "Sucess", 0, [System.Windows.Forms.MessageBoxIcon]::Information)               
+                }
+                catch {
+                    [System.Windows.Forms.MessageBox]::Show("Unable to create Veeam Configuration backup", "Error", 0, [System.Windows.Forms.MessageBoxIcon]::Exclamation)        
+                }
+            }
             if ($Global:BackupCounter -eq "True") {
+                $PB_Status1.Value = 100
+                Start-Sleep -Seconds 1.5
                 $Form.Dispose()
             }
             else {
-                $BU_Start1.Text = "Start"
+                #$BU_Start1.Text = "Start"
+                $PB_Status1.Value = 0
             }
         }
     })
@@ -364,14 +413,3 @@ $BU_Cancel1.Add_Click( {
         $Form.Dispose()
     })
 [void]$Form.ShowDialog()
-
-<#
-$CurrentConfigBackup = Get-VBRConfigurationBackupJob
-$CurrentRepository = $CurrentConfigBackup.Target
-$LocalServer = Get-VBRServer | where {($_.Type -eq "Local") -and ($_.Description -eq "Backup server")}
-$TempRepository = Add-VBRBackupRepository -Name "Temp_Repo_$(get-date -f dd.MM.yyyy_HH.mm.ss)" -Server $LocalServer -Folder "C:\install" -Type WinLocal
-
-Set-VBRConfigurationBackupJob -Enable:$true -Repository $TempRepository
-Start-VBRConfigurationBackupJob
-Set-VBRConfigurationBackupJob -Repository $CurrentRepository
-Remove-VBRBackupRepository -Repository $TempRepository -Confirm:$false#>
